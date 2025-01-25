70ff992f6da3c8e7f30f0d45e3423fe870b83502
gventurini <giacomo.venturini3@gmail.com>
2025-01-25 21:04:59 +0100

    add put /task-category

--- src/main/java/ch/cern/todo/controller/TaskCategoriesController.java
+++ src/main/java/ch/cern/todo/controller/TaskCategoriesController.java
@@ -2,14 +2,18 @@ package ch.cern.todo.controller;
 
 import ch.cern.todo.model.api.CreateTaskCategoryApi;
 import ch.cern.todo.model.api.TaskCategoryApi;
+import ch.cern.todo.model.api.UpdateTaskCategoryApi;
 import ch.cern.todo.model.business.TaskCategory;
 import ch.cern.todo.model.mapper.TaskCategoriesMapper;
 import ch.cern.todo.service.TaskCategoriesService;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +32,22 @@ public class TaskCategoriesController {
             .build();
 
         val result = taskCategoriesService.createTaskCategory(input);
+
+        val apiResult = taskCategoriesMapper.businessToApi(result);
+
+        return new ResponseEntity<>(apiResult, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/task-category/{categoryId}")
+    public ResponseEntity<TaskCategoryApi> updateTaskCategory(@PathVariable Long categoryId,
+                                                              @Validated @RequestBody UpdateTaskCategoryApi body) {
+        val input = TaskCategory.builder()
+            .categoryId(categoryId)
+            .categoryName(body.getCategoryName())
+            .categoryDescription(body.getCategoryDescription())
+            .build();
+
+        val result = taskCategoriesService.updateTaskCategory(input);
 
         val apiResult = taskCategoriesMapper.businessToApi(result);
 
--- src/main/java/ch/cern/todo/service/TaskCategoriesService.java
+++ src/main/java/ch/cern/todo/service/TaskCategoriesService.java
@@ -6,6 +6,7 @@ import ch.cern.todo.repository.TaskCategoriesRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +19,22 @@ public class TaskCategoriesService {
     @Transactional
     public TaskCategory createTaskCategory(TaskCategory taskCategory) {
         val taskCategoryEntity = taskCategoriesMapper.businessToEntity(taskCategory);
+
+        taskCategoriesRepository.save(taskCategoryEntity);
+
+        return taskCategoriesMapper.entityToBusiness(taskCategoryEntity);
+    }
+
+    @Transactional
+    public TaskCategory updateTaskCategory(TaskCategory taskCategory) {
+        val taskCategoryEntity = taskCategoriesRepository.getReferenceById(taskCategory.getCategoryId());
+
+        if (StringUtils.isNotBlank(taskCategory.getCategoryName())) {
+            taskCategoryEntity.setCategoryName(taskCategory.getCategoryName());
+        }
+        if (StringUtils.isNotBlank(taskCategory.getCategoryDescription())) {
+            taskCategoryEntity.setCategoryDescription(taskCategory.getCategoryDescription());
+        }
 
         taskCategoriesRepository.save(taskCategoryEntity);
 
