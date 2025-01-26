a957c725004c671e763c3f828083362e7139ec0e
gventurini <giacomo.venturini3@gmail.com>
2025-01-26 22:19:43 +0100

    add utility to easily map to a model spring session information

---
+++ src/main/java/ch/cern/todo/model/business/LoggedUserInfo.java
@@ -0,0 +1,12 @@
+package ch.cern.todo.model.business;
+
+import java.util.Set;
+
+public record LoggedUserInfo(Long userId,
+                             String username,
+                             String firstname,
+                             String lastname,
+                             // right now for simplicity the application only supports one role per user,
+                             // but it is best to foresee the support for multiple roles
+                             Set<Role> roles) {
+}
---
+++ src/main/java/ch/cern/todo/util/LoggedUserUtils.java
@@ -0,0 +1,44 @@
+package ch.cern.todo.util;
+
+import ch.cern.todo.configuration.security.CernAuthenticatedUser;
+import ch.cern.todo.model.business.LoggedUserInfo;
+import ch.cern.todo.model.business.Role;
+import lombok.experimental.UtilityClass;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@UtilityClass
+public class LoggedUserUtils {
+
+    public static LoggedUserInfo getLoggedUserInfo() {
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+            .map(SecurityContext::getAuthentication)
+            .map(Authentication::getPrincipal)
+            .map(principal -> {
+                if (principal instanceof CernAuthenticatedUser user) {
+                    return user;
+                }
+                return null;
+            }).map(cernAuthenticatedUser -> new LoggedUserInfo(
+                cernAuthenticatedUser.getUserId(), cernAuthenticatedUser.getUsername(),
+                cernAuthenticatedUser.getFirstname(), cernAuthenticatedUser.getLastname(),
+                authoritiesToRoles(cernAuthenticatedUser.getAuthorities())
+            ))
+            .orElseThrow(); // TODO: define a custom exception
+    }
+
+    private static Set<Role> authoritiesToRoles(Collection<GrantedAuthority> authorities) {
+        return CollectionUtils.emptyIfNull(authorities).stream()
+            .map(GrantedAuthority::getAuthority)
+            .map(Role::valueOf)
+            .collect(Collectors.toUnmodifiableSet());
+    }
+}
