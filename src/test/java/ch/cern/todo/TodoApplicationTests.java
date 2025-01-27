c104e2ef17b3b2574a28b16b09334b8ee3a4ae4e
gventurini <giacomo.venturini3@gmail.com>
2025-01-27 12:07:14 +0100

    add log in context loads

--- src/test/java/ch/cern/todo/TodoApplicationTests.java
+++ src/test/java/ch/cern/todo/TodoApplicationTests.java
@@ -1,13 +1,17 @@
 package ch.cern.todo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class TodoApplicationTests {
 
 	@Test
 	void contextLoads() {
+        log.info("Application started successfully");
 	}
 
 }
