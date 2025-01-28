3fbf2f132027f4dac1c73a801a681775dd1fceec
gventurini <giacomo.venturini3@gmail.com>
2025-01-28 15:57:14 +0100

    add getTasks tests for user and admin

--- src/test/java/ch/cern/todo/integration/controller/TaskControllerTest.java
+++ src/test/java/ch/cern/todo/integration/controller/TaskControllerTest.java
@@ -9,7 +9,9 @@ import ch.cern.todo.model.api.CreateTaskApi;
 import ch.cern.todo.model.api.UpdateTaskApi;
 import ch.cern.todo.model.business.LoggedUserInfo;
 import ch.cern.todo.model.business.Role;
+import ch.cern.todo.model.business.SearchTask;
 import ch.cern.todo.model.business.Task;
+import ch.cern.todo.model.mapper.PageMapper;
 import ch.cern.todo.service.TaskService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -43,6 +45,9 @@ class TaskControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoSpyBean
+    private PageMapper pageMapper;
 
     @MockitoSpyBean
     private TaskService taskService;
@@ -132,9 +137,52 @@ class TaskControllerTest {
         verify(taskService, times(1)).deleteTask(1L, adminLoggedUserInfo());
     }
 
+    @Test
+    void getTasks_AsAdmin_ShouldReturnTasksAssignedToAnyUser() throws Exception {
+        mockMvc.perform(get("/v1/task")
+                .headers(HttpHeadersUtil.getAdminHeaders()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.content").isArray())
+            .andExpect(jsonPath("$.content.length()").value(10))
+            .andExpect(jsonPath("$.content[0].assignedToUserInfo.userId").value(1))
+            .andExpect(jsonPath("$.content[1].assignedToUserInfo.userId").value(1))
+            .andExpect(jsonPath("$.content[2].assignedToUserInfo.userId").value(1))
+            .andExpect(jsonPath("$.content[3].assignedToUserInfo.userId").value(1))
+            .andExpect(jsonPath("$.content[4].assignedToUserInfo.userId").value(2))
+            .andExpect(jsonPath("$.content[5].assignedToUserInfo.userId").value(2))
+            .andExpect(jsonPath("$.content[6].assignedToUserInfo.userId").value(1))
+            .andExpect(jsonPath("$.content[7].assignedToUserInfo.userId").value(3))
+            .andExpect(jsonPath("$.content[8].assignedToUserInfo.userId").value(3))
+            .andExpect(jsonPath("$.content[9].assignedToUserInfo.userId").value(3));
+
+        verify(taskService, times(1)).getTasks(any(SearchTask.class), eq(adminLoggedUserInfo()));
+        verify(pageMapper, times(1)).businessToApi(any(), any());
+    }
+
+    @Test
+    void getTasks_AsUser_ShouldReturnTasksAssignedOnlyToThatUser() throws Exception {
+        mockMvc.perform(get("/v1/task")
+                .param("username", "bobUser") // this should get ignored
+                .headers(HttpHeadersUtil.getAliceHeaders()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.content").isArray())
+            .andExpect(jsonPath("$.content.length()").value(2))
+            .andExpect(jsonPath("$.content[0].assignedToUserInfo.userId").value(2))
+            .andExpect(jsonPath("$.content[1].assignedToUserInfo.userId").value(2));
+
+        verify(taskService, times(1)).getTasks(any(SearchTask.class), eq(aliceLoggedUserInfo()));
+        verify(pageMapper, times(1)).businessToApi(any(), any());
+    }
+
     private LoggedUserInfo adminLoggedUserInfo() {
         return new LoggedUserInfo(
             1L, "cernAdmin", "Cern", "Admin", Set.of(Role.ROLE_ADMIN)
+        );
+    }
+
+    private LoggedUserInfo aliceLoggedUserInfo() {
+        return new LoggedUserInfo(
+            2L, "aliceUser", "Alice", "Ikari", Set.of(Role.ROLE_USER)
         );
     }
 }
