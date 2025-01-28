f376d7053518ee2344afd07bb6190698cc57e3ab
gventurini <giacomo.venturini3@gmail.com>
2025-01-28 22:33:01 +0100

    add missing serial version uuid

--- src/main/java/ch/cern/todo/model/business/CernException.java
+++ src/main/java/ch/cern/todo/model/business/CernException.java
@@ -3,8 +3,12 @@ package ch.cern.todo.model.business;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import java.io.Serial;
+
 // TODO: Error messages should use a more structured approach, like using error codes (i.e. using enums) and support localization
 public class CernException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 9103378622070575067L;
 
     @Getter
     private final int statusCode;
